8bf588a238bc8e344877da56ef4b770a44e25682
root <root@localhost>
2021-09-30 20:17:12 +0530

    auto publish 1633013231967

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+A= int(input("Enter Your First Number>"))
+
+B= int(input("Enter Your Second Number>"))
+
+C= input("Enter Your Operation")
+
+if '+' in C:
+    print(A + B)
+elif '-' in C: 
+    print(A -B) 
+elif "*" in C: 
+    print(A * B) 
+elif '/' in C:
+    print(A / B)
